6879b9eac6e0a157188bc49af3cda5a4a245b4fe
sc454 <sc454@duke.edu>
2017-03-01 21:36:14 -0500

    Navigation Drawer with options

---
+++ navigationDrawer/app/src/main/java/com/example/cheli/navigationdrawer/CourseActivity.java
@@ -0,0 +1,9 @@
+package com.example.cheli.navigationdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cheli on 3/1/2017.
+ */
+public class CourseActivity extends AppCompatActivity {
+}
