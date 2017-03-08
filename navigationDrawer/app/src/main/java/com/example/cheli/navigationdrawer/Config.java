73f39e0a174d7ba8790c30dc0a8311bc880c71f8
branchvincent <branchevincent@gmail.com>
2017-03-07 20:46:58 -0500

    Removed old project

--- navigationDrawer/app/src/main/java/com/example/cheli/navigationdrawer/Config.java
+++
@@ -1,9 +0,0 @@
-package com.example.cheli.navigationdrawer;
-
-/**
- * Created by johnb on 3/3/2017.
- */
-
-public class Config {
-    public static final String FIREBASE_URL = "https://dbnav-4be47.firebaseio.com/";
-}
