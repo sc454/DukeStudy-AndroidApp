33948784382503ecc8f8c2fc251c7b9bb74fef38
branchvincent <branchevincent@gmail.com>
2017-03-27 22:19:16 -0400

    Updated strings

--- DukeStudy/app/src/main/java/com/ds/DukeStudy/SignupActivity.java
+++ DukeStudy/app/src/main/java/com/ds/DukeStudy/SignupActivity.java
@@ -1,32 +1,4 @@
 package com.ds.DukeStudy;
-
-// import android.os.Bundle;
-// import android.support.design.widget.FloatingActionButton;
-// import android.support.design.widget.Snackbar;
-// import android.support.v7.app.AppCompatActivity;
-// import android.support.v7.widget.Toolbar;
-// import android.view.View;
-//
-// public class SignupActivity extends AppCompatActivity {
-//
-//     @Override
-//     protected void onCreate(Bundle savedInstanceState) {
-//         super.onCreate(savedInstanceState);
-//         setContentView(R.layout.activity_signup);
-//         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//         setSupportActionBar(toolbar);
-//
-//         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//         fab.setOnClickListener(new View.OnClickListener() {
-//             @Override
-//             public void onClick(View view) {
-//                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                         .setAction("Action", null).show();
-//             }
-//         });
-//     }
-//
-// }
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -65,12 +37,12 @@ public class SignupActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
 
-//        btnResetPassword.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
-//            }
-//        });
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
+            }
+        });
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
