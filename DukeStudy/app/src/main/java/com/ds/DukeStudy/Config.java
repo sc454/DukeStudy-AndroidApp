31d50fab28d1a502c440bb3abc090713384029ea
branchvincent <branchevincent@gmail.com>
2017-04-03 21:17:47 -0400

    Added general methods to write objects to database

--- DukeStudy/app/src/main/java/com/ds/DukeStudy/Config.java
+++
@@ -1,9 +0,0 @@
-package com.ds.DukeStudy;
-
-/**
- * Created by johnb on 3/3/2017.
- */
-//This fragment contains the URL for our firebase database.
-public class Config {
-    public static final String FIREBASE_URL = "https://dukestudy-a11a3.firebaseio.com";
-}
