87276b8bfd227c80ede3a4678ee68ddce0b7afaf
John Bralich <John Bralich>
2017-04-06 22:02:48 -0400

    Added reading single objects from database in members fragment

--- DukeStudy/app/src/main/java/com/ds/DukeStudy/GroupsListFragment.java
+++ DukeStudy/app/src/main/java/com/ds/DukeStudy/GroupsListFragment.java
@@ -1,5 +1,6 @@
 package com.ds.DukeStudy;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -31,11 +33,22 @@ public class GroupsListFragment extends Fragment {
         DatabaseReference postsRef=databaseRef.child("groups");//This needs to be changed dynamically
         adapter1=new FirebaseListAdapter<Group>(getActivity(),Group.class,android.R.layout.two_line_list_item,postsRef) {
             @Override
-            protected void populateView(View v, Group model, int position) {
+            protected void populateView(View v, final Group model, int position) {
                 TextView mytext=(TextView) v.findViewById(android.R.id.text1);
-                TextView mytext1=(TextView) v.findViewById(android.R.id.text1);
+                TextView mytext1=(TextView) v.findViewById(android.R.id.text2);
                 mytext.setText(model.getName());
                 mytext1.setText("Member Count: "+Integer.toString(model.getStudentKeys().size()));
+                v.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Context context = getActivity().getApplicationContext();
+                        CharSequence text = model.getKey();
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                });
             }
 
         };
